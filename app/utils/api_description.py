cc4f7b7448f4abf69f36b6866de4b64e6e8461f9
joec11 <jc625@njit.edu>
2024-07-04 11:52:41 -0400

    Removed unnecessary directories and files from the project.

--- app/utils/api_description.py
+++
@@ -1,8 +0,0 @@
-def getDescription():
-    description = """
-Application Overview:
-
-This application is a Rest API made with FastApi that implements RAG (Retrieval Augmented Generation).\n
-The user enters a query and receives a response based on that query.
-"""
-    return description
