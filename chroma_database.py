96a059185b753a4a5b481812c3435d7ce7922c1f
joec11 <jc625@njit.edu>
2024-07-04 21:38:51 -0400

    Modified main.py and chroma_database.py by implementing logging.

--- chroma_database.py
+++ chroma_database.py
@@ -11,6 +11,7 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
+from configure_logging import createLogger
 
 # Load environment variables
 load_dotenv(find_dotenv())
@@ -22,14 +23,18 @@ openai.api_key = os.getenv('OPENAI_API_KEY', 'default_openai_api_key')
 CHROMA_PATH = os.getenv('CHROMA_DIR', 'chroma')
 DATA_PATH = os.getenv('DATA_DIR', 'data')
 
+logging = createLogger(os.path.basename(__file__))
+
 def create():
     delete()
     documents = load_documents()
     chunks = split_text(documents)
     save_to_chroma(chunks)
+    logging.info("Chroma: create()")
 
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    logging.info("Chroma: load_documents()")
     return loader.load()
 
 def split_text(documents):
@@ -41,12 +46,14 @@ def split_text(documents):
     )
     chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    logging.info("Chroma: split_text()")
     return chunks
 
 def save_to_chroma(chunks):
     # Clear out the database first.
     if os.path.exists(CHROMA_PATH): \
-        shutil.rmtree(CHROMA_PATH)
+        shutil.rmtree(CHROMA_PATH); \
+        logging.info("Chroma: save_to_chroma() - Cleared the database")
 
     # Create a new DB from the documents.
     db = Chroma.from_documents(
@@ -54,10 +61,13 @@ def save_to_chroma(chunks):
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+    logging.info("Chroma: save_to_chroma() - Saved chunks to the chroma directory")
 
 def delete():
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
         print(f"Deleted Chroma directory at {CHROMA_PATH}.")
+        logging.info("Chroma: delete() is true")
     else:
         print(f"Chroma directory at {CHROMA_PATH} does not exist.")
+        logging.info("Chroma: delete() is false")
